60f8eb7191e8b69381c891177dfe01299aaaadd5
Aiym <ashimhanova_a@ourlook.com>
2026-01-23 02:33:57 +0500

    new

--- src/com/company/data/interfaces/IDB.java
+++ src/com/company/data/interfaces/IDB.java
@@ -6,3 +6,4 @@ public interface IDB {
     void close();
 }
 
+
--- src/com/company/repositories/interfaces/ICourseRepository.java
+++ src/com/company/repositories/interfaces/ICourseRepository.java
@@ -5,6 +5,7 @@ import com.company.models.Course;
 import java.util.List;
 
 
+
 public interface ICourseRepository {
     boolean createCourse(Course course);
     Course getCourse(int id);
